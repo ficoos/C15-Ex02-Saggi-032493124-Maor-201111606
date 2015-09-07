2be57a9614f181b4fb4b76d43e86a195c1de676e
Saggi Mizrahi <ficoos@gmail.com>
2015-09-07 17:10:53 +0300

    stuff

---
+++ Facebooky/PostFilterFactory.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace Facebooky
+{
+	public static class PostFilterFactory
+	{
+		private static readonly Dictionary<string, Type> sr_FactoryMapping = new Dictionary<string, Type>();
+
+		static PostFilterFactory()
+		{
+			const bool v_Inherit = true;
+			Assembly myAssembly = typeof(SerializerFactory).Assembly;
+			foreach (Type type in myAssembly.GetAllTypesWithAttribute(typeof(RegisterAttribute), !v_Inherit))
+			{
+				foreach (RegisterAttribute registerAttribute in type.GetCustomAttributes(typeof(RegisterAttribute), !v_Inherit).Cast<RegisterAttribute>())
+				{
+					sr_FactoryMapping.Add(registerAttribute.Kind, type);
+				}
+			}
+		}
+
+		public class RegisterAttribute : Attribute
+		{
+			public string Kind { get; set; }
+
+			public RegisterAttribute(string i_Kind)
+			{
+				Kind = i_Kind;
+			} 
+		}
+
+		public static IEnumerable<string> Kinds
+		{
+			get
+			{
+				return sr_FactoryMapping.Keys;
+			}
+		}
+
+		public static IPostFilter CreatePostFilter(string i_Kind, string i_Data)
+		{
+			ConstructorInfo constructorInfo = sr_FactoryMapping[i_Kind].GetConstructor(new[] { typeof(string) });
+			IPostFilter result = null;
+			if (constructorInfo != null)
+			{
+				result = constructorInfo.Invoke(new object[] { i_Data }) as IPostFilter;
+			}
+
+			return result;
+		}
+	}
+}
---
+++ Facebooky/WordPostFilter.cs
@@ -0,0 +1,83 @@
+﻿using System.Xml;
+using System.Xml.Schema;
+
+using FacebookWrapper.ObjectModel;
+
+namespace Facebooky
+{
+	[PostFilterFactory.Register("Word Filter")]
+	public class WordPostFilter : IPostFilter
+	{
+		private RegexPostFilter m_PostFilter;
+
+		private string m_Word;
+
+		public string Word
+		{
+			get
+			{
+				return m_Word;
+			}
+
+			set
+			{
+				m_Word = value;
+				string regex = string.Format(@"(?-i)\b{0}\b", m_Word);
+				m_PostFilter = new RegexPostFilter(regex);
+			}
+		}
+
+		public WordPostFilter() : this(string.Empty)
+		{
+		}
+
+		public WordPostFilter(string i_Word)
+		{
+			Word = i_Word;
+			Enabled = true;
+		}
+
+		public XmlSchema GetSchema()
+		{
+			return null;
+		}
+
+		public void ReadXml(XmlReader i_Reader)
+		{
+			string rawWord = i_Reader.GetAttribute("Word");
+			Word = rawWord ?? string.Empty;
+			string rawEnabled = i_Reader.GetAttribute("Enabled");
+			if (rawEnabled == null)
+			{
+				Enabled = false;
+			}
+			else
+			{
+				Enabled = bool.Parse(rawEnabled);
+			}
+
+			i_Reader.ReadStartElement();
+		}
+
+		public void WriteXml(XmlWriter i_Writer)
+		{
+			i_Writer.WriteAttributeString("Word", Word);
+			i_Writer.WriteAttributeString("Enabled", Enabled.ToString());
+		}
+
+		public bool IsMatch(Post i_Post)
+		{
+			return m_PostFilter.IsMatch(i_Post);
+		}
+
+		public bool Enabled { get; set; }
+
+		public string Name
+		{
+			get
+			{
+				return Word;
+			}
+		}
+	}
+}
