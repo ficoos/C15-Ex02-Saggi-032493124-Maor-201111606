af47826a0a1120b0ce9c6425026bc838ccbd12d1
Maor <maor.sade@gmail.com>
2015-09-14 21:51:16 +0300

    Shortcuts setting form UI

--- Facebooky/FormSohrtcutsSettings.cs
+++ Facebooky/FormSohrtcutsSettings.cs
@@ -1,11 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Collections.Specialized;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
 using System.Windows.Forms;
 
 namespace Facebooky
@@ -13,25 +7,46 @@ namespace Facebooky
 	public partial class FormShortcutsSettings : Form
 	{
 		private readonly StringDictionary r_ShortcutsDickDictionary;
+
 		public FormShortcutsSettings(StringDictionary i_ShortcutsDickDictionary)
 		{
 			r_ShortcutsDickDictionary = i_ShortcutsDickDictionary;
 			InitializeComponent();
+			foreach (string key in r_ShortcutsDickDictionary.Keys)
+			{
+				listBoxShortcutsList.Items.Add(key);
+			}
+			listBoxShortcutsList.SelectedIndexChanged += listBoxShortcutsListOnSelectedIndexChanged;
 		}
 
-		private void buttonAdd_Click(object sender, EventArgs e)
+		private void listBoxShortcutsListOnSelectedIndexChanged(object i_Sender, EventArgs i_EventArgs)
 		{
-			if (isInputLegal(textBoxFrom.Text,textBoxTo.Text))
+			if (listBoxShortcutsList.SelectedItem != null)
+			{
+				textBox1.Text = r_ShortcutsDickDictionary[listBoxShortcutsList.SelectedItem.ToString()];
+			}
+			else
+			{
+				{
+					textBox1.Text = "";
+				}
+			}
+		}
+
+		private void buttonAdd_Click(object i_Sender, EventArgs i_Args)
+		{
+			if (isInputLegal(textBoxFrom.Text, textBoxTo.Text))
 			{
 				r_ShortcutsDickDictionary.Add(textBoxFrom.Text, textBoxTo.Text);
-			}		
+				listBoxShortcutsList.Items.Add(textBoxFrom.Text);
+			}
 		}
 
 		private bool isInputLegal(string i_From, string i_To)
 		{
 			bool legalReplace = !r_ShortcutsDickDictionary.ContainsKey(i_From);
 
-			if (i_From == null || i_To == null ||  0 <= i_To.IndexOf(i_From))
+			if (i_From == null || i_To == null || 0 <= i_To.IndexOf(i_From))
 			{
 				legalReplace = false;
 			}
@@ -46,6 +61,13 @@ namespace Facebooky
 				}
 			}
 			return legalReplace;
+		}
+
+		private void btnRemoveSelected_Click(object i_Sender, EventArgs i_Args)
+		{
+			string selectedKey = listBoxShortcutsList.SelectedItem.ToString();
+			r_ShortcutsDickDictionary.Remove(selectedKey);
+			listBoxShortcutsList.Items.Remove(selectedKey);
 		}
 	}
 }
